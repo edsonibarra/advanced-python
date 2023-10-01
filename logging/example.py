45a3735a5e8a38e262ac6ffd1a87f3455427b918
edsonibarra <edsonibarra97@gmail.com>
2023-09-30 18:49:05 -0600

    example

---
+++ logging/example.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+
+
+logger = logging.getLogger(__name__)
+stream_handler = logging.StreamHandler(sys.stdout)
+logger.addHandler(stream_handler)
+
+
+def division():
+  try:
+    dividend = float(input("Enter the dividend: "))
+    logger.info(dividend)
+    divisor = float(input("Enter the divisor: "))
+    logger.info(divisor)
+    if divisor == 0.0:
+      logger.log(logging.ERROR, "Attempting to divide by 0!")
+  except ValueError:
+    logger.log(logging.CRITICAL, "No dividend or divisor value entered!")
+    return None
+  if divisor == 0:
+    return None
+  else:
+    return dividend/divisor
+result = division()
+if result is None:
+  logger.warning("The result value is None!")
